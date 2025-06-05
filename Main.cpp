12bf117c3a07d5c635cc14d7476c4f6f599d5a49
Joshua Feldman <154766797+12joshuaf@users.noreply.github.com>
2025-06-05 11:17:22 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,42 +1,68 @@
 #include "Node.hpp"
-#include <iostream>
-#include <vector>
+#include "Tensor.hpp"
 
 int main() {
     using namespace nn;
 
-    // Create a node with 3 inputs, using Sigmoid activation, in "HiddenLayer1", named "Node1", and of type Hidden
-    Node node(3, ActivationFunction::Sigmoid, "HiddenLayer1", "Node1", NodeType::Hidden);
+    // Create two nodes: input and output
+    Node input_node(
+        2,                          // 2 inputs
+        ActivationFunction::ReLU,  // Activation function
+        "InputLayer",              // Layer name
+        "InputNode1",              // Node name
+        NodeType::Hidden           // Node type
+    );
 
-    // Sample input vector
-    std::vector<double> input = { 0.5, -0.3, 0.8 };
+    Node output_node(
+        1,                          // 1 input (output of input_node)
+        ActivationFunction::Sigmoid, // Activation function
+        "OutputLayer",              // Layer name
+        "OutputNode1",              // Node name
+        NodeType::Output            // Node type
+    );
 
-    // Forward activation
-    double output = node.activate(input);
-    std::cout << "Activation output: " << output << "\n";
+    // Connect the input node to the output node
 
-    // Simulated error signal
-    double error = 0.2;
-    double learning_rate = 0.05;
+    input_node.point_node(&output_node);
 
-    // Backpropagation step
-    node.backpropagate(error, learning_rate, input);
-
-    // Show parameters after one backpropagation step
-    node.print_parameters();
-
-    // Demonstrate safe backpropagation
-    for (int i = 0; i < 15; ++i) {
-        node.safe_backpropagate(error, learning_rate, input);
-    }
-
-
-    node.print_parameters();
+    // Feed dummy input data into input_node
+    std::vector<double> dummy_input = { 0.5, -0.2 };
 
 
 
 
+    // Set input_node inputs and activate
+    input_node.inputs = dummy_input;
 
+    input_node.print_parameters();
+    output_node.print_parameters();
+
+    std::cout << '\n';
+
+
+    double input_output = input_node.activate();
+
+    std::cout << "Output of InputNode: " << input_output << '\n';
+
+    double input2 = output_node.inputs[0];
+
+    std::cout << "Input of outputnode: " << input2 <<  '\n';
+
+    double final_activate = output_node.activate();
+
+    std::cout << "Node activation: " << final_activate << std::endl;
+
+
+    output_node.backpropagate(1,0.5,10);
+    input_node.backpropagate(.5, 10);
+
+
+    input_node.backpropagate(.5, 10);
+
+    std::cout << '\n';
+
+    input_node.print_parameters();
+    output_node.print_parameters();
 
 
     std::cout << "Press any key to continue\n";
