dcabb42a05d1c863db7319c38d3fb4263a30a4e2
Joshua Feldman <154766797+12joshuaf@users.noreply.github.com>
2025-06-17 13:47:54 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,58 +1,45 @@
-#include <iostream>
-#include <vector>
-#include "Layer.hpp"
+#include "Net.hpp"
 
-using namespace nn;
+
 
 int main() {
-    std::cout << "=== Layer Test with Dummy Data ===\n";
+    nn::Net net;
 
+    net.add_layer(4, 3, nn::ActivationFunction::ReLU, nn::NodeType::Hidden);
+    net.add_layer(2, 4, nn::ActivationFunction::Sigmoid, nn::NodeType::Output);
 
+    std::vector<nn::Layer*> layers = {
+        net.get_layer(0),
+        net.get_layer(1)
+    };
 
-    Layer hidden_layer(2, 3, ActivationFunction::ReLU, "Hidden", NodeType::Hidden);
+    std::cout << "Before training:\n";
+    net.print_parameters();
 
-    // Input vector (3 inputs)
-    std::vector<double> input = { 1.0, 0.5, -0.5 };
+    std::vector<double> input = { 0.5, 0.1, -0.4 };
 
-    // Activate the hidden layer
-    hidden_layer.activate(input);
+    layers[0]->activate(input);
+    std::vector<double> hidden_output = layers[0]->get_outputs();
 
-    // Print hidden layer outputs
-    std::cout << "Hidden layer outputs:\n";
-    for (const auto& out : hidden_layer.get_outputs()) {
-        std::cout << out << " ";
-    }
-    std::cout << "\n";
+    layers[1]->activate(hidden_output);
+    std::vector<double> output = layers[1]->get_outputs();
 
-    // Create an output layer with 2 nodes (to match hidden layer output)
-    Layer output_layer(2, 2, ActivationFunction::Sigmoid, "Output", NodeType::Output);
+    std::vector<double> target = { 1.0, 0.0 };
 
-    // Activate output layer using output from hidden layer
-    output_layer.activate(hidden_layer.get_outputs());
-
-    std::cout << "Output layer predictions:\n";
-    for (const auto& out : output_layer.get_outputs()) {
-        std::cout << out << " ";
-    }
-    std::cout << "\n";
-
-    // Dummy targets to test backprop
-    std::vector<double> targets = { 0.0, 1.0 };
-
-    // Backpropagate through output layer
     double learning_rate = 0.1;
-    int saturation_threshold = 1000;
+    int saturation_threshold = 5;
 
-    std::cout << "Backpropagating output layer...\n";
-    output_layer.backpropagate(targets, learning_rate, saturation_threshold);
+    layers[1]->backpropagate(target, learning_rate, saturation_threshold);
+    layers[0]->backpropagate(learning_rate, saturation_threshold);
 
-    // Backpropagate through hidden layer
-    std::cout << "Backpropagating hidden layer...\n";
-    hidden_layer.backpropagate(learning_rate, saturation_threshold);
+    std::cout << "\nAfter training:\n";
+    net.print_parameters();
 
-    std::cout << "Test complete.\n";
+    std::cout << "Backpropagation completed.\n";
 
+    std::cout << "Press any key to continue...";
     std::cin.get();
 
     return 0;
 }
+
